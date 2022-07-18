52103f9b0bfcc02fd63e45f38cd279effd869208
jubaer ibn zohir <jubaeribnzohir@gmail.com>
2022-07-19 00:47:37 +0600

    starting the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>hello world.</h1>
+      <h1>hello world.</h1>
+      
     </div>
   );
 }
