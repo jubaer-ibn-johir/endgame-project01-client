42683e8eb28684b9c3d2d5b15d560999ee5e4fcf
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-07-19 21:03:06 +0600

    home page create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Pages/Home";
 import Navbar from "./components/shared/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Home> </Home>
     </div>
   );
 }
